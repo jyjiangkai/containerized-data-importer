d8b20b8754cb1336fc2384d1d19beaaf16674706
kai.jiang <kai.jiang@easystack.cn>
2021-12-02 14:44:23 +0800

    update

--- hack/build/build-es.sh
+++ hack/build/build-es.sh
@@ -53,6 +53,9 @@ aarch64* | arm64*)
 esac
 
 
+dnf install -y libnbd-devel nbdkit-devel
+
+
 # handle binaries
 
 eval "$(go env)"
@@ -67,7 +70,19 @@ elif [ "${target}" = "cdi-importer" ]; then
 elif [ "${target}" = "cdi-operator" ]; then
     go build -a -o ${OUTPUT_DIR}/csv-generator tools/csv-generator/csv-generator.go && chmod +x ${OUTPUT_DIR}/csv-generator
 elif [ "${target}" = "cdi-uploadserver" ]; then
-    sudo dnf install libnbd-devel.x86_64
+    # sudo dnf install libnbd-devel.x86_64
+    wegt http://mirrors.163.com/fedora/updates/33/Everything/x86_64/Packages/l/libnbd-1.6.5-1.fc33.x86_64.rpm -O /tmp/libnbd-1.6.5-1.fc33.x86_64.rpm
+    wegt http://mirrors.163.com/fedora/updates/33/Everything/x86_64/Packages/q/qemu-img-5.1.0-9.fc33.x86_64.rpm -O /tmp/qemu-img-5.1.0-9.fc33.x86_64.rpm
+    wegt http://mirrors.163.com/fedora/updates/33/Everything/x86_64/Packages/x/xen-libs-4.14.3-2.fc33.x86_64.rpm -O /tmp/xen-libs-4.14.3-2.fc33.x86_64.rpm
+    wegt https://mirrors.tuna.tsinghua.edu.cn/fedora/releases/33/Everything/x86_64/os/Packages/l/libaio-0.3.111-10.fc33.x86_64.rpm -O /tmp/libaio-0.3.111-10.fc33.x86_64.rpm
+    wegt https://mirrors.tuna.tsinghua.edu.cn/fedora/releases/33/Everything/x86_64/os/Packages/c/capstone-4.0.2-3.fc33.x86_64.rpm -O /tmp/capstone-4.0.2-3.fc33.x86_64.rpm
+    wegt http://mirrors.163.com/fedora/updates/33/Everything/x86_64/Packages/l/liburing-0.7-3.fc33.x86_64.rpm -O /tmp/liburing-0.7-3.fc33.x86_64.rpm
+    rpm -ivh /tmp/libnbd-1.6.5-1.fc33.x86_64.rpm
+    rpm -ivh /tmp/qemu-img-5.1.0-9.fc33.x86_64.rpm
+    rpm -ivh /tmp/xen-libs-4.14.3-2.fc33.x86_64.rpm
+    rpm -ivh /tmp/libaio-0.3.111-10.fc33.x86_64.rpm
+    rpm -ivh /tmp/capstone-4.0.2-3.fc33.x86_64.rpm
+    rpm -ivh /tmp/liburing-0.7-3.fc33.x86_64.rpm
 fi
 
 
